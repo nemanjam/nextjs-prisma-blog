52e2829723423d269d48ab71ee0dcb6bceab6d72
nemanjam <nemanja.mitic.elfak@hotmail.com>
2020-06-21 19:11:35 +0200

    seed in progress

--- server/seeds/seed.ts
+++ server/seeds/seed.ts
@@ -4,7 +4,21 @@ import * as faker from 'faker';
 const prisma = new PrismaClient();
 
 const main = async () => {
-  console.log(faker.name.findName());
+  const user1 = await prisma.user.create({
+    data: {
+      bio: faker.lorem.sentences(2),
+      email: faker.internet.email(),
+      image: faker.image.avatar(),
+      password: 'password',
+      username: faker.internet.userName,
+      role: 'ADMIN',
+      //   favorites: [],
+      //   followedBy: [],
+      //   following: [],
+      //   comments: [],
+      //   articles: [],
+    },
+  });
 };
 
 main()
